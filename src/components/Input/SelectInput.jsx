26d8af107c5f02d9a382b2b7fcc25a025dc63569
Kavin Paul <kavinp622@gmail.com>
2026-02-24 21:52:46 -0700

    add select input and clean up input components

--- src/components/Input/SelectInput.jsx
+++ src/components/Input/SelectInput.jsx
@@ -1,35 +1,37 @@
 import "./Input.scss";
 
-const Input = ({
-    type = "text",
-    placeholder = "",
+const SelectInput = ({
     label,
     value,
     onChange,
+    options = [],
     className = "",
     error = "",
     disabled = false,
     ...props
 }) => {
-
     let combinedClassName = "input";
     if (className) combinedClassName += " " + className;
 
     return (
         <div className="input-wrapper">
             {label && <label className="input-label">{label}</label>}
-            <input
-                type={type}
+            <select
                 className={`${combinedClassName} ${error ? "input--error" : ""}`}
-                placeholder={placeholder}
                 value={value}
                 onChange={onChange}
                 disabled={disabled}
                 {...props}
-            />
+            >
+                {options.map((opt, index) => (
+                    <option key={index} value={opt.value}>
+                        {opt.label}
+                    </option>
+                ))}
+            </select>
             <p className="input__error-msg">{error}</p>
         </div>
     );
 };
 
-export default Input;
+export default SelectInput;
