f19cd3fc7f4e20be179a2274f050bcefd58a5105
Kavin Paul <kavinp622@gmail.com>
2026-04-05 23:15:23 -0600

    update delete modal componenet to ts

--- src/components/DeleteModal/DeleteModal.tsx
+++ src/components/DeleteModal/DeleteModal.tsx
@@ -1,23 +1,28 @@
 import { useEffect } from "react";
-import Typography from "../Typography/Typography.jsx";
-import Button from "../Button/Button.jsx";
-import Iconography from "../Iconography/Iconography.jsx";
+import Typography from "../Typography/Typography";
+import Button from "../Button/Button";
+import Iconography from "../Iconography/Iconography";
 import "./DeleteModal.scss";
+
+interface DeleteModalProps {
+  deleteItem: string;
+  variant?: "expenses";
+  onCancel: () => void;
+  onConfirm: () => void;
+}
 
 const DeleteModal = ({
   deleteItem,
   variant = "expenses",
   onCancel,
   onConfirm,
-}) => {
+}: DeleteModalProps) => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
       document.body.style.overflow = "auto";
     };
   }, []);
-
-  const safeVariant = variant == "expenses" ? variant : "expenses";
 
   return (
     <div className="delete-modal__wrapper">
@@ -30,14 +35,14 @@ const DeleteModal = ({
           <Iconography name="close" />
         </button>
 
-        {safeVariant == "expenses" && (
+        {variant === "expenses" && (
           <>
             <Typography variant="h1" className="delete-modal__title">
               Delete {deleteItem} expense?
             </Typography>
             <Typography variant="p1" className="delete-modal__text">
               Please confirm that you’d like to delete the {deleteItem}
-              expense from the list of expenses.You won’t be able to undo this
+              expense from the list of expenses. You won’t be able to undo this
               action.
             </Typography>
           </>
