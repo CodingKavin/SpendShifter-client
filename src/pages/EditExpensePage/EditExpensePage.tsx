0c0f1cab62fa88c6392fc2f184d39cb01d1edefc
Kavin Paul <kavinp622@gmail.com>
2026-04-23 21:13:35 -0600

    update Edit Expense Page to ts

--- src/pages/EditExpensePage/EditExpensePage.tsx
+++ src/pages/EditExpensePage/EditExpensePage.tsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import api from "../../utils/axios.js";
-import ExpenseForm from "../../components/ExpenseForm/ExpenseForm.jsx";
+import api from "../../utils/axios";
+import ExpenseForm from "../../components/ExpenseForm/ExpenseForm";
+import type {ExpenseFormData, Expense} from "../../types/types";
 
 const EditExpensePage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [initialData, setInitialData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [initialData, setInitialData] = useState<ExpenseFormData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchExpense = async () => {
       try {
-        const res = await api.get(`/expenses/${id}`);
+        const res = await api.get<Expense>(`/expenses/${id}`);
         const expense = res.data;
-        if (expense.date) {
-          expense.date = expense.date.split("T")[0];
-        }
-        setInitialData(expense);
-      } catch (error) {
+
+        const formattedDate = expense.date.split("T")[0];
+
+        setInitialData({...expense, date: formattedDate ?? "", amount: String(expense.amount)});
+      } catch (error: any) {
         console.error("Error fetching expense:", error);
       } finally {
         setLoading(false);
@@ -28,9 +29,13 @@ const EditExpensePage = () => {
     fetchExpense();
   }, [id]);
 
-  const handleSubmit = async (formData) => {
+  const handleSubmit = async (formData: Partial<ExpenseFormData>) => {
     try {
-      await api.patch(`/expenses/${id}`, formData);
+      const payload = {
+      ...formData,
+      amount: Number(formData.amount)
+    };
+      await api.patch(`/expenses/${id}`, payload);
       navigate("/expenses");
     } catch (error) {
       console.error("Error updating expense:", error);
